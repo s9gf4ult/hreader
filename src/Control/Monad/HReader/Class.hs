6f4bbea4a7ce268a1f6f1b7abde3e53a46833d37
Aleksey Uimanov <s9gf4ult@gmail.com>
2015-06-30 00:36:34 +0500

    add: haskM and firends

--- src/Control/Monad/HReader/Class.hs
+++ src/Control/Monad/HReader/Class.hs
@@ -13,10 +13,21 @@ import Control.Monad.Error
 #endif
 
 
-
-class MonadHReader m where
+#if MIN_VERSION_base(4, 8, 0)
+class (Monad m) => MonadHReader m where
+#else
+class (Monad m, Applicative m) => MonadHReader m where
+#endif
   type HSetElements m :: [*]
   askHSet :: m (HSet (HSetElements m))
+
+haskM :: (MonadHReader m, Contains (HSetElements m) e)
+      => m e
+haskM = hget <$> askHSet
+
+haskLabeledM :: (MonadHReader m, Contains (HSetElements m) (Labeled label e))
+             => proxy label -> m e
+haskLabeledM p = hgetLabeled p <$> askHSet
 
 instance (Monad m, MonadHReader m) => MonadHReader (ReaderT r m) where
   type HSetElements (ReaderT r m) = HSetElements m
