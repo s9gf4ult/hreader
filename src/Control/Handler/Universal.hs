efde4e653a3c52bc7b726925b59b0f8d2f961f08
Aleksey Uimanov <s9gf4ult@gmail.com>
2015-06-30 00:04:47 +0500

    implementing ...

--- src/Control/Handler/Universal.hs
+++ src/Control/Handler/Universal.hs
@@ -1,9 +1,12 @@
 module Control.Handler.Universal where
 
 import Control.Handler.Universal.Class
+import Control.Monad.Base
 import Control.Monad.Cont
+import Control.Monad.Morph
 import Control.Monad.Reader
 import Control.Monad.State
+import Control.Monad.Trans.Control
 import Control.Monad.Writer
 import Data.HSet
 
@@ -13,27 +16,44 @@ import Control.Monad.Except
 import Control.Monad.Error
 #endif
 
-newtype UniversalT els m a = UniversalT
-    { unUniversal :: ReaderT (HSet els) m a
+newtype HReaderT els m a = HReaderT
+    { unHReaderT :: ReaderT (HSet els) m a
     } deriving ( Functor, Applicative, Monad
                , MonadError e, MonadCont, MonadWriter w
-               , MonadState s )
+               , MonadState s, MonadBase b )
 
-instance MonadTrans (UniversalT els) where
-  lift = UniversalT . lift
+runUnversalT :: HSet els -> HReaderT els m a -> m a
+runUnversalT h (HReaderT r) = runReaderT r h
 
-instance (MonadReader r m) => MonadReader r (UniversalT els m) where
+instance MonadTrans (HReaderT els) where
+  lift = HReaderT . lift
+
+instance (MonadReader r m) => MonadReader r (HReaderT els m) where
   ask = lift ask
-  local f ma = UniversalT $ do
+  local f ma = HReaderT $ do
     h <- ask
     lift $ do
       local f $ runUnversalT h ma
 
-instance (Monad m) => HasHSet (UniversalT els m) where
-  type HSetElements (UniversalT els m) = els
-  askHSet = UniversalT ask
+instance (Monad m) => HasHSet (HReaderT els m) where
+  type HSetElements (HReaderT els m) = els
+  askHSet = HReaderT ask
 
-deriving instance (HasHSet m) => HasHSet (ReaderT r m)
+deriving instance MFunctor (HReaderT els)
+deriving instance MMonad (HReaderT els)
 
-runUnversalT :: HSet els -> UniversalT els m a -> m a
-runUnversalT h (UniversalT r) = runReaderT r h
+#if MIN_VERSION_monad_control(1, 0, 0)
+instance MonadTransControl (HReaderT els) where
+  type StT (HReaderT els) a = StT (ReaderT (HSet els)) a
+  liftWith action = HReaderT $ do
+    liftWith $ \runTrans -> action (runTrans . unHReaderT)
+  restoreT = HReaderT . restoreT
+
+instance (MonadBaseControl b m) => MonadBaseControl b (HReaderT els m) where
+  type StM (HReaderT els m) a = StM (ReaderT (HSet els) m) a
+  liftBaseWith action = HReaderT $ do
+    liftBaseWith $ \runInBase -> action (runInBase . unHReaderT)
+  restoreM = HReaderT . restoreM
+#else
+
+#endif
--- src/Control/Handler/Universal/Class.hs
+++ src/Control/Handler/Universal/Class.hs
@@ -1,8 +1,23 @@
 module Control.Handler.Universal.Class where
 
 import Data.HSet
+import Control.Monad.Cont
+import Control.Monad.Reader
+import Control.Monad.State
+import Control.Monad.Writer
+
+#if MIN_VERSION_mtl(2, 2, 1)
+import Control.Monad.Except
+#else
+import Control.Monad.Error
+#endif
+
 
 
 class HasHSet m where
   type HSetElements m :: [*]
   askHSet :: m (HSet (HSetElements m))
+
+instance (Monad m, HasHSet m) => HasHSet (ReaderT r m) where
+  type HSetElements (ReaderT r m) = HSetElements m
+  askHSet = lift askHSet
